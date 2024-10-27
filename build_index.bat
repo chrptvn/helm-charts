c4c5163dde0c6f1c846b6854218c19d085190b92
cacpot <christian.potvin@vanderlande.com>
2024-10-27 19:03:48 -0400

    added roommanager

--- build_index.bat
+++ build_index.bat
@@ -1,4 +1,5 @@
 helm package ./assistant --destination ./
 helm package ./invokeai --destination ./
 helm package ./agentmanager --destination ./
+helm package ./roommanager --destination ./
 helm repo index . --url https://chrptvn.github.io/helm-charts/
