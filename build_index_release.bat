1778bcf7f9df9a9321a28082ec5b5435456bf2b5
cacpot <christian.potvin@vanderlande.com>
2025-02-02 19:51:45 -0500

    Update Helm Chart

---
+++ build_index_release.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: Combine base version, timestamp, and SNAPSHOT
+set version=1.0.0
+helm package ./gymbro-be --destination ./ --version %version%
+helm package ./gymbro-ui --destination ./ --version %version%
+helm package ./image-servlet --destination ./ --version %version%
+helm repo index . --url https://chrptvn.github.io/helm-charts/
+
+git add -A
+git commit -m "Update Helm Chart"
+git push
